a4d6a9c3c8b9a60bdd70224996bff4d6d510e21e
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-10-17 11:25:19 -0400

    Inception Model is utilized.

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage !")
+            }
+            
+            // calling method
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    func detect(image : CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML failed !")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process !")
+            }
+            print(results)
+        }
+        
+        
+        // creating a handler for image request
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }
+        catch{
+            print(error.localizedDescription)
+        }
+        
+        
     }
 
-
+    @IBAction func cameraTyped(_ sender: Any) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
